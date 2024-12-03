f3940f8987939ae1b49dac64284f5312e646c794
FaisalAnsari <faisalansari30121998@gmail.com>
2024-12-03 16:40:34 +0530

    commit

---
+++ src/app/components/marque/Marque.jsx
@@ -0,0 +1,14 @@
+// components/Marquee.js
+import React from 'react';
+
+const Marquee = () => {
+  return (
+    <div className="marquee-container">
+      <div className="marquee-content">
+        🎉 Special Offer: Flat 30% off on all sofas! | Free delivery on orders above $500 🚛 | Buy now and pay later with 0% interest! 🛋️
+      </div>
+    </div>
+  );
+};
+
+export default Marquee;
